e2af3001327c8913ce58beb809cca5af93ab4bb5
VictoriaTatarinova <chiarouomo@gmail.com>
2022-05-19 19:41:56 +0200

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 200; // Изначальная сумма на счете
-        int x = 1200; // Сумма пополнения
+        int payment = 1000; // Сумма пополнения
 
         int sumVSBonus;
-        if (x >= 1000) {
-            sumVSBonus = amount + x + x / 100; // Общая сумма, включая бонус
-            int totalBonus = x / 100; // Сумма бонуса
+        if (payment > 1000) {
+            sumVSBonus = amount + payment + payment / 100; // Общая сумма, включая бонус
+            int totalBonus = payment / 100; // Сумма бонуса
 
             System.out.println("Сумма " + sumVSBonus);
             System.out.println("Итоговый бонус " + totalBonus);
 
         } else {
-            sumVSBonus = amount + x;
+            sumVSBonus = amount + payment;
             System.out.println("Сумма " + sumVSBonus);
             System.out.println("Итоговый бонус 0");
         }
